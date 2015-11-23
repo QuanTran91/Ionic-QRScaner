cd5f83bd693a030315d7e0d4d26448eacd175cfe
Quan Tran <hoangquan1991@gmail.com>
2015-11-23 21:51:35 +0700

    Add date picker example

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,32 @@
 angular.module('starter.controllers', [])
 
-.controller('ScanCtrl', function($scope,$cordovaBarcodeScanner) {
+.controller('ScanCtrl', function($scope,$cordovaBarcodeScanner,$cordovaDatePicker) {
   $scope.buttonName = "Click to scan QR code"; 
    $scope.scan = function(){
     $cordovaBarcodeScanner.scan().then(function(imageData){
       $scope.textResult = imageData.text;
+      alert(imageData.text);
     },function(error){
-      console.log("Error");
+      alert("Error");
     });
+  };
+  
+  $scope.pickDate = function(){
+     var options = {
+    date: new Date(),
+    mode: 'date', // or 'time'
+    minDate: new Date() - 10000,
+    allowOldDates: true,
+    allowFutureDates: false,
+    doneButtonLabel: 'DONE',
+    doneButtonColor: '#F2F3F4',
+    cancelButtonLabel: 'CANCEL',
+    cancelButtonColor: '#000000'
+  };
+  
+  $cordovaDatePicker.show(options).then(function(date){
+        alert(date);
+    });
+
   };
 });
